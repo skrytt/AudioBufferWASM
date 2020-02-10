1620e8a2773a17f0b904469b5146a7800ad08b65
Skrytt <matt.kemp.1992@gmail.com>
2020-02-10 22:12:08 +0000

    Add WASM and glue files

---
+++ pkg/audio_buffer_wasm.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class Data {
+  free(): void;
+/**
+*/
+  constructor();
+/**
+*/
+  generate_white_noise(): void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {RequestInfo | BufferSource | WebAssembly.Module} module_or_path
+*
+* @returns {Promise<any>}
+*/
+export default function init (module_or_path?: RequestInfo | BufferSource | WebAssembly.Module): Promise<any>;
+        
---
+++ pkg/audio_buffer_wasm_bg.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_data_free(a: number): void;
+export function data_new(): number;
+export function data_generate_white_noise(a: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_exn_store(a: number): void;
